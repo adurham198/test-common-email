b0e8eedbe80eb17d00b0867fd82b86e820fe923f
adurham198 <33640838+adurham198@users.noreply.github.com>
2021-04-05 16:10:43 -0400

    Added testGetMailSession call to EmailTest.java

--- commons-email-1.3.2-src 2/src/test/java/EmailTest.java
+++ commons-email-1.3.2-src 2/src/test/java/EmailTest.java
@@ -165,5 +165,46 @@ public class EmailTest {
 				return new PasswordAuthentication(username, password);
 			}
 		});
+		email.setMailSession(session);
+		
+		assertEquals("smtp.gmail.com", email.getHostName());
+    }
+    	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testGetMailSession() throws Exception {
+		final String username = "username@gmail.com";
+		final String password = "password";
+		
+		Properties props = new Properties();
+				
+		props.put(EmailConstants.MAIL_SMTP_AUTH, "TRUE");
+		props.put(EmailConstants.MAIL_HOST, "smtp.gmail.com");
+		props.put(EmailConstants.MAIL_PORT, "465");
+		props.put(EmailConstants.MAIL_SMTP_USER, "username@gmail.com");
+		props.put(EmailConstants.MAIL_SMTP_PASSWORD, "password");
+		props.put(EmailConstants.MAIL_SMTP_SSL_ENABLE, 1);
+		props.put(EmailConstants.MAIL_SMTP_SSL_CHECKSERVERIDENTITY, 0);
+		props.put(EmailConstants.MAIL_SMTP_CONNECTIONTIMEOUT, "110000");
+		props.put(EmailConstants.MAIL_SMTP_CONNECTIONTIMEOUT, "default");
+		props.put(EmailConstants.SOCKET_TIMEOUT_MS, 100000);
+		props.put(EmailConstants.EMAIL_BODY, "TestBody");
+		props.put(EmailConstants.SMTP, "596");
+		props.put(EmailConstants.MAIL_SMTP_SSL_SOCKET_FACTORY_PORT, "593");
+		//props.put(email.bounceAddress, null);
+		props.setProperty(EmailConstants.MAIL_SMTP_SOCKET_FACTORY_CLASS, "New");
+		props.setProperty(EmailConstants.MAIL_SMTP_SOCKET_FACTORY_FALLBACK, "True");
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+		
+		//email.setMailSession(session);
+		
+		//assertEquals(email.hostName, email.getHostName());
+		
+	}
+
 
 }
