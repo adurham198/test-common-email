244a7ccecb5e002438310e64809a376b4b605a0a
adurham198 <33640838+adurham198@users.noreply.github.com>
2021-04-05 16:04:41 -0400

    Added testAddCC() function test call

--- commons-email-1.3.2-src 2/src/test/java/EmailTest.java
+++ commons-email-1.3.2-src 2/src/test/java/EmailTest.java
@@ -41,4 +41,11 @@ public class EmailTest {
 		
 		assertEquals(3, email.getBccAddresses().size());
 	}
+    @Test
+	public void testAddCc() throws Exception {
+		email.addCc(TEST_EMAIL);
+		
+		assertEquals(3, email.getCcAddresses().size());
+		
+	}
 }
